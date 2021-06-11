763036f07881a0afa040d561ac333542e56ee7e1
Ty-Sir <tylersiravo@gmail.com>
2021-06-11 15:45:23 -0400

    Update create.js

--- client/assets/js/create.js
+++ client/assets/js/create.js
@@ -6,7 +6,3 @@ let user = Moralis.User.current();
 $('#singleButton').click(() =>{
   window.location.href='erc-721.html';
 });
-
-$('#multipleButton').click(() =>{
-  // window.location.href='erc-1155.html';
-});
