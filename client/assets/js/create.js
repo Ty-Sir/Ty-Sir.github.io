565a1b57bbf43794d7ca999db8012ad947de9018
Ty-Sir <tylersiravo@gmail.com>
2021-06-11 15:05:32 -0400

    initial push

---
+++ client/assets/js/create.js
@@ -0,0 +1,12 @@
+Moralis.initialize("WrQJkJZs66S7Dp31Hm5Ujtn1o5uZ3pzZTmZmLOok"); // Application id from moralis.io
+Moralis.serverURL = 'https://l5qznev3yhuw.moralis.io:2053/server'; //Server url from moralis.io
+
+let user = Moralis.User.current();
+
+$('#singleButton').click(() =>{
+  window.location.href='erc-721.html';
+});
+
+$('#multipleButton').click(() =>{
+  // window.location.href='erc-1155.html';
+});
---
+++ client/assets/js/learn.js
@@ -0,0 +1,4 @@
+Moralis.initialize("WrQJkJZs66S7Dp31Hm5Ujtn1o5uZ3pzZTmZmLOok"); // Application id from moralis.io
+Moralis.serverURL = 'https://l5qznev3yhuw.moralis.io:2053/server'; //Server url from moralis.io
+
+const user = Moralis.User.current();
